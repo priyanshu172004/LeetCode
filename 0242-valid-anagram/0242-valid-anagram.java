7c6d1b837dbc1f824330ca685446ec423b5140b6
Priyanshu Srivastava <151062186+priyanshu172004@users.noreply.github.com>
2026-01-10 01:25:36 +0530

    Time: N/A (0%), Space: N/A (0%) - LeetHub

--- 0242-valid-anagram/0242-valid-anagram.java
+++ 0242-valid-anagram/0242-valid-anagram.java
@@ -8,7 +8,7 @@ class Solution {
             map.put(ch, map.getOrDefault(ch, 0) + 1);
         }
         for(char ch : t.toCharArray()){
-            if(!map.containsKey(ch) || map.get(ch) == 0){
+            if(!map.containsKey(ch)){
                 return false;
             }
             map.put(ch, map.get(ch) - 1);
