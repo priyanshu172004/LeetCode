a299b4f1dbd24f4f5884b261c8ce3378cc5367ac
Priyanshu Srivastava <151062186+priyanshu172004@users.noreply.github.com>
2026-01-10 01:20:54 +0530

    Time: 15 ms (24.54%), Space: 46.6 MB (26.16%) - LeetHub

---
+++ 0242-valid-anagram/0242-valid-anagram.java
@@ -0,0 +1,19 @@
+class Solution {
+    public boolean isAnagram(String s, String t) {
+        if(s.length() != t.length()){
+            return false;
+        }
+        HashMap<Character, Integer> map = new HashMap<>();
+        HashMap<Character, Integer> map2 = new HashMap<>();
+        for(char ch : s.toCharArray()){
+            map.put(ch, map.getOrDefault(ch, 0) + 1);
+        }
+        for(char ch : t.toCharArray()){
+            if(!map.containsKey(ch) || map.get(ch) == 0){
+                return false;
+            }
+            map.put(ch, map.get(ch) - 1);
+        }
+        return true;
+    }
+}
