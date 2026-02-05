4ed2be55cd07593ec6e91942646f75b83c4815b6
Priyanshu Srivastava <151062186+priyanshu172004@users.noreply.github.com>
2026-02-06 00:46:49 +0530

    Time: 1 ms (98.72%), Space: 46.3 MB (96.47%) - LeetHub

--- 3379-transformed-array/3379-transformed-array.java
+++ 3379-transformed-array/3379-transformed-array.java
@@ -3,20 +3,9 @@ class Solution {
         int n = nums.length;
         int[] result = new int[n];
         for(int i = 0; i < n; i++){
-            result[i] = 0;
-        }
-        for(int i = 0; i < nums.length; i++){
             int step = nums[i];
-            if(step < 0){
-                int prevIndex = ((i + step) % n + n) % n;
-                result[i] = nums[prevIndex];
-
-            }else if(step > 0){
-                int nextIndex = (i + step) % n;
-                result[i] = nums[nextIndex];
-            }else{
-                result[i] = nums[i];
-            }
+            int idx = ((i + step) % n + n) % n;
+            result[i] = nums[idx];
         }
         return result;
     }
