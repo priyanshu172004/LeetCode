b3e6e94b5c47bde6dc18e0fa989782ebb7dd4919
Priyanshu Srivastava <151062186+priyanshu172004@users.noreply.github.com>
2026-02-17 14:26:48 +0530

    Time: 1 ms (17.77%), Space: 43.5 MB (82.15%) - LeetHub

--- 0063-unique-paths-ii/0063-unique-paths-ii.java
+++ 0063-unique-paths-ii/0063-unique-paths-ii.java
@@ -1,27 +1,25 @@
 class Solution {
-    public int findPaths(int[][] dp, int[][] grid, int m, int n){
-        if(grid[0][0] == 1) return 0;
-        dp[0][0] = 1;
-        for(int i = 1; i < m; i++){
-            if(grid[i][0] == 0){
-                dp[i][0] = dp[i - 1][0];
-            }else{
-                dp[i][0] = 0;
-            }
-        }
-        for(int j = 1; j < n; j++){
-            if(grid[0][j] == 0){
-                dp[0][j] = dp[0][j - 1];
-            }else{
-                dp[0][j] = 0;
-            }
-        }
-        for(int i = 1; i <= m - 1; i++){
-            for(int j = 1; j <= n - 1; j++){
+    public int findUniquePaths(int m, int n, int[][] dp, int[][] grid){
+        if (grid[0][0] == 1) return 0;
+        for(int i = 0; i <= m - 1; i++){
+            for(int j = 0; j <= n - 1; j++){
                 if(grid[i][j] == 1){
                     dp[i][j] = 0;
-                }else{
-                    dp[i][j] = dp[i - 1][j] + dp[i][j - 1];
+                    continue;
+                }
+                else if(i == 0 && j == 0){
+                    dp[i][j] = 1;
+                }
+                else{
+                    int up = 0;
+                    int left = 0;
+                    if(i > 0){
+                        up = dp[i - 1][j];
+                    }
+                    if(j > 0){
+                        left = dp[i][j - 1];
+                    }
+                    dp[i][j] = up + left;
                 }
             }
         }
@@ -31,6 +29,6 @@ class Solution {
         int m = obstacleGrid.length;
         int n = obstacleGrid[0].length;
         int[][] dp = new int[m][n];
-        return findPaths(dp, obstacleGrid, m, n);
+        return findUniquePaths(m, n, dp, obstacleGrid);
     }
 }
