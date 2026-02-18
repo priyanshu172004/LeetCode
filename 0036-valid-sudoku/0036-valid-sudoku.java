99d5eccce1dba2577aa756060974ccac07e5ce44
Priyanshu Srivastava <151062186+priyanshu172004@users.noreply.github.com>
2026-02-18 23:27:57 +0530

    Time: 1 ms (99.79%), Space: 46.4 MB (82.91%) - LeetHub

--- 0036-valid-sudoku/0036-valid-sudoku.java
+++ 0036-valid-sudoku/0036-valid-sudoku.java
@@ -25,7 +25,6 @@ class Solution {
     public boolean isValidSudoku(char[][] board) {
         for(int i = 0; i < 9; i++){
             for(int j = 0; j < 9; j++){
-
                 if(board[i][j] != '.'){
                     char digit = board[i][j];
                     board[i][j] = '.';
