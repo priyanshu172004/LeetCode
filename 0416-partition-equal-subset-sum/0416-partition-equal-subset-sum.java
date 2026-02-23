ae03eef6c9a70ca9d3b52e64c2dd16beac847200
Priyanshu Srivastava <151062186+priyanshu172004@users.noreply.github.com>
2026-02-23 17:56:20 +0530

    Time: 58 ms (40.79%), Space: 76.1 MB (15.15%) - LeetHub

--- 0416-partition-equal-subset-sum/0416-partition-equal-subset-sum.java
+++ 0416-partition-equal-subset-sum/0416-partition-equal-subset-sum.java
@@ -1,20 +1,25 @@
 class Solution {
-    public static boolean findIfTargetExists(int n, int target, int[] nums, int[][] dp){
+    public static boolean findIfTargetExists(int n, int target, int[] nums){
+        int[][] dp = new int[n + 1][target + 1];
         if(target == 0){
+            for(int i = 0; i <= n; i++){
+                dp[i][0] = 1;
+            }
             return true;
         }
-        if(n == 0){
-            return nums[0] == target;
+        if(nums[0] <= target){
+            dp[0][nums[0]] = 1;
         }
-        if(dp[n][target] != -1){
-            return dp[n][target] == 1;
+        for(int i = 1; i <= n; i++){
+            for(int tar = 1; tar <= target; tar++){
+                boolean notPick = (dp[i - 1][tar] == 1);
+                boolean pick = false;
+                if(nums[i] <= tar){
+                    pick = (dp[i - 1][tar - nums[i]] == 1);
+                }
+                dp[i][tar] = (pick || notPick) ? 1 : 0;
+            }
         }
-        boolean notPick = findIfTargetExists(n - 1, target, nums, dp);
-        boolean pick = false;
-        if(nums[n] <= target){
-            pick = findIfTargetExists(n - 1, target - nums[n], nums, dp);
-        }
-        dp[n][target] = (pick || notPick) ? 1 : 0;
         return dp[n][target] == 1;
     }
     public boolean canPartition(int[] nums) {
@@ -26,10 +31,6 @@ class Solution {
             return false;
         }
         int sum = totalSum/2;
-        int[][] dp = new int[nums.length + 1][sum + 1];
-        for(int[] row : dp){
-            Arrays.fill(row, -1);
-        }
-        return findIfTargetExists(nums.length - 1, sum, nums, dp);
+        return findIfTargetExists(nums.length - 1, sum, nums);
     }
 }
