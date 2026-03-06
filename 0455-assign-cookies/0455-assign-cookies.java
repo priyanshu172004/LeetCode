aaa6972a9ea583c8455a674d5699590a8b24696e
Priyanshu Srivastava <151062186+priyanshu172004@users.noreply.github.com>
2026-03-07 00:31:01 +0530

    Time: 14 ms (93.07%), Space: 51.3 MB (87.05%) - LeetHub

---
+++ 0455-assign-cookies/0455-assign-cookies.java
@@ -0,0 +1,16 @@
+class Solution {
+    public int findContentChildren(int[] g, int[] s) {
+        int n = g.length;
+        int m = s.length;
+        Arrays.sort(g);
+        Arrays.sort(s);
+        int left = 0, right = 0;
+        while(left < m && right < n){
+            if(g[right] <= s[left]){
+                right++;
+            }
+            left++;
+        }
+        return right;
+    }
+}
