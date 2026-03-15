769d8779d29571d748bb3bf50ab13a3a76728429
Priyanshu Srivastava <151062186+priyanshu172004@users.noreply.github.com>
2026-03-15 15:49:52 +0530

    Time: 3 ms (86.08%), Space: 47.8 MB (47.54%) - LeetHub

--- 0135-candy/0135-candy.java
+++ 0135-candy/0135-candy.java
@@ -1,27 +1,29 @@
 class Solution {
+    //Slope(Peek-Down) Approach
     public int candy(int[] ratings) {
         int n = ratings.length;
-        int[] left = new int[n];
-        int[] right = new int[n];
-        left[0] = 1;
-        right[n - 1] = 1;
-        for(int i = 1; i < n; i++){
-            if(ratings[i] > ratings[i - 1]){
-                left[i] = left[i - 1] + 1;
-            }else{
-                left[i] = 1;
+        int i = 1;
+        int sum = 1;
+        while(i < n){
+            if(ratings[i] == ratings[i - 1]){
+                sum += 1;
+                i++;
             }
-        }
-        for(int i = n - 2; i >= 0; i--){
-            if(ratings[i] > ratings[i + 1]){
-                right[i] = right[i + 1] + 1;
-            }else{
-                right[i] = 1;
+            int peek = 1;
+            while(i < n && ratings[i] > ratings[i - 1]){
+                peek += 1;
+                sum += peek;
+                i++;
             }
-        }
-        int sum = 0;
-        for(int i = 0; i < n; i++){
-            sum += Math.max(left[i], right[i]);
+            int down = 1;
+            while(i < n && ratings[i] < ratings[i - 1]){
+                sum += down;
+                i++;
+                down = down + 1;
+            }
+            if(down > peek){
+                sum += (down - peek);
+            }
         }
         return sum;
     }
