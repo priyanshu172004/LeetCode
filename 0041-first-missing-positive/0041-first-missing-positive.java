32d8f74e83d655e57453f66ee5adb1ce25058cfa
Priyanshu Srivastava <151062186+priyanshu172004@users.noreply.github.com>
2026-04-22 19:24:46 +0530

    Time: 14 ms (25.23%), Space: 90.7 MB (12%) - LeetHub

--- 0041-first-missing-positive/0041-first-missing-positive.java
+++ 0041-first-missing-positive/0041-first-missing-positive.java
@@ -5,16 +5,13 @@ class Solution {
         for(int num : nums){
             if(num > 0) set.add(num);
         }
-
         int num = 1;
-
         while(num <= nums.length + 1){
             if(!set.contains(num)){
                 return num;
             }
             num++;
         }
-
         return -1;
     }
 }
