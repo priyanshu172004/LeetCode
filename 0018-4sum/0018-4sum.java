9b1a4a8e1e5970bbc58b478db1c0a958b21bfa54
Priyanshu Srivastava <151062186+priyanshu172004@users.noreply.github.com>
2026-04-25 16:32:38 +0530

    Time: 20 ms (66.8%), Space: 45.6 MB (77.66%) - LeetHub

--- 0018-4sum/0018-4sum.java
+++ 0018-4sum/0018-4sum.java
@@ -3,8 +3,8 @@ class Solution {
         Arrays.sort(nums);
         List<List<Integer>> result = new ArrayList<>();
         for(int i = 0; i < nums.length - 3; i++){
+            if(i > 0 && nums[i] == nums[i - 1]) continue;
             for(int j = i + 1; j < nums.length - 2; j++){
-                if(i > 0 && nums[i] == nums[i - 1]) continue;
                 if(j > i + 1 && nums[j] == nums[j - 1]) continue;
                 int left = j + 1;
                 int right = nums.length - 1;
