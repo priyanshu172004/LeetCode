35bb5250ceb2b102878cc90264d0d6c6a163a86b
Priyanshu Srivastava <151062186+priyanshu172004@users.noreply.github.com>
2026-05-01 02:25:52 +0530

    Time: 369 ms (29.03%), Space: 270 MB (13.98%) - LeetHub

---
+++ 3742-maximum-path-score-in-a-grid/3742-maximum-path-score-in-a-grid.java
@@ -0,0 +1,46 @@
+class Solution {
+
+    Integer[][][] dp;
+
+    public int findScore(int[][] grid, int row, int col, int k) {
+        int m = grid.length, n = grid[0].length;
+
+        if (row >= m || col >= n) return Integer.MIN_VALUE;
+
+        int val = grid[row][col];
+
+        int cost = (val == 0) ? 0 : 1;
+        int score = val;
+
+        // if cost exceeds
+        if (k < cost) return Integer.MIN_VALUE;
+
+        // destination
+        if (row == m - 1 && col == n - 1) {
+            return score;
+        }
+
+        if (dp[row][col][k] != null) return dp[row][col][k];
+
+        int right = findScore(grid, row, col + 1, k - cost);
+        int down = findScore(grid, row + 1, col, k - cost);
+
+        int maxNext = Math.max(right, down);
+
+        if (maxNext == Integer.MIN_VALUE) {
+            return dp[row][col][k] = Integer.MIN_VALUE;
+        }
+
+        return dp[row][col][k] = score + maxNext;
+    }
+
+    public int maxPathScore(int[][] grid, int k) {
+        int m = grid.length, n = grid[0].length;
+
+        dp = new Integer[m][n][k + 1];
+
+        int ans = findScore(grid, 0, 0, k);
+
+        return ans == Integer.MIN_VALUE ? -1 : ans;
+    }
+}
