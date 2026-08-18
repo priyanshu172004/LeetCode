cd1689a9de08428ec414e0cb5ad06474bc909eb5
Priyanshu Srivastava <151062186+priyanshu172004@users.noreply.github.com>
2026-08-18 16:01:05 +0530

    Time: 3 ms (66.61%), Space: 19.3 MB (32.79%) - LeetHub

--- 1408-string-matching-in-an-array/1408-string-matching-in-an-array.py
+++ 1408-string-matching-in-an-array/1408-string-matching-in-an-array.py
@@ -1,8 +1,7 @@
 class Solution:
     def stringMatching(self, words: List[str]) -> List[str]:
         ans = []
-        words = sorted(words)
-        print(words)
+        
         left = 0
         for i in range(len(words)):
             for j in range(len(words)):
