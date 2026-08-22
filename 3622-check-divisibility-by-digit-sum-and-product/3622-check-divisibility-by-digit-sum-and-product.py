d444e93415abd5e4c3ee1275515eab064f004adf
Priyanshu Srivastava <151062186+priyanshu172004@users.noreply.github.com>
2026-08-22 22:54:05 +0530

    Time: 0 ms (100%), Space: 19.4 MB (5.35%) - LeetHub

--- 3622-check-divisibility-by-digit-sum-and-product/3622-check-divisibility-by-digit-sum-and-product.py
+++ 3622-check-divisibility-by-digit-sum-and-product/3622-check-divisibility-by-digit-sum-and-product.py
@@ -2,12 +2,10 @@ class Solution:
     def checkDivisibility(self, n: int) -> bool:
         summ = 0
         product = 1
-        cummulative = 0
         for digit in str(n):
             summ += int(digit)
             product *= int(digit)
-        cummulative = summ + product
-        if n % cummulative == 0:
+        if n % (summ + product) == 0:
             return True
         return False
 
