dc602815142c3f70c8f2772b1302765c71da9f45
Priyanshu Srivastava <151062186+priyanshu172004@users.noreply.github.com>
2026-09-08 10:54:52 +0530

    Time: 102 ms (17.56%), Space: 19.6 MB (69.17%) - LeetHub

---
+++ 1160-find-words-that-can-be-formed-by-characters/1160-find-words-that-can-be-formed-by-characters.py
@@ -0,0 +1,19 @@
+class Solution:
+    def countCharacters(self, words: List[str], chars: str) -> int:
+        hashMap = {}
+        temp = {}
+        summ = 0
+        for ch in chars:
+            hashMap[ch] = hashMap.get(ch, 0) + 1
+            temp[ch] = temp.get(ch, 0) + 1
+        for word in words:
+            countLength = len(word)
+            count = 0
+            for char in word:
+                if char in hashMap and hashMap[char] > 0:
+                    hashMap[char] = hashMap.get(char) - 1
+                    count += 1
+                    if countLength == count:
+                        summ += countLength
+            hashMap = temp.copy()
+        return summ
