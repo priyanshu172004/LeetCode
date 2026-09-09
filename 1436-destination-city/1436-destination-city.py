fb29d1e15042afaa7e8bd870146f895dfa4ce78e
Priyanshu Srivastava <151062186+priyanshu172004@users.noreply.github.com>
2026-09-09 13:59:50 +0530

    Time: 0 ms (100%), Space: 19.3 MB (28.19%) - LeetHub

---
+++ 1436-destination-city/1436-destination-city.py
@@ -0,0 +1,17 @@
+class Solution:
+    def destCity(self, paths: List[List[str]]) -> str:
+        current_pair = paths[0]
+        start = current_pair[0]
+        end = current_pair[1]
+
+        while True:
+            found_next = False
+            for pair in paths:
+                if pair[0] == end:
+                    start = pair[0]
+                    end = pair[1]
+                    found_next = True
+                    break
+            if not found_next:
+                return end
+        return ""
