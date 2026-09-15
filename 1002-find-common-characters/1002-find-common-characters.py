505cf4d97280a020bba2b376c85fe5f0747bf13d
Priyanshu Srivastava <151062186+priyanshu172004@users.noreply.github.com>
2026-09-15 12:18:31 +0530

    Time: 11 ms (48.55%), Space: 19.3 MB (69.22%) - LeetHub

--- 1002-find-common-characters/1002-find-common-characters.py
+++ 1002-find-common-characters/1002-find-common-characters.py
@@ -1,22 +1,26 @@
 class Solution:
     def commonChars(self, words: List[str]) -> List[str]:
         hashMap = {}
-        for word in range(0, 1):
-            for char in words[word]:
-                hashMap[char] = hashMap.get(char, 0) + 1
+
+        for char in words[0]:
+            hashMap[char] = hashMap.get(char, 0) + 1
+
+        for word in words[1:]:
+            temp = {}
+
+            for char in word:
+                temp[char] = temp.get(char, 0) + 1
+
+            for key in hashMap:
+                if key in temp:
+                    hashMap[key] = min(hashMap[key], temp[key])
+                else:
+                    hashMap[key] = 0
 
         result = []
-        for key, value in hashMap.items():
-            count = value
-            for word in words:
-                temp = {}
-                for char in word:
-                    temp[char] = temp.get(char, 0) + 1
-                if key not in temp:
-                    count = 0
-                    break
-                count = min(count, temp[key])
-            for i in range(count):
-                result.append(key)
-        return result
 
+        for key, value in hashMap.items():
+            for i in range(value):
+                result.append(key)
+
+        return result
